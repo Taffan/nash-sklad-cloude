1de063553db3e59c7d402db54435b7b4d0c0b38c
Taffan <taffan@gmail.com>
2026-03-14 23:41:28 +0300

    Update create_webview_project.py

--- scripts/create_webview_project.py
+++ scripts/create_webview_project.py
@@ -146,6 +146,48 @@ public class MainActivity extends Activity {{
 
  private static final int CAMERA_REQUEST = 1;
 
+class JSBridge {
+
+ Activity activity;
+
+ JSBridge(Activity a){
+  activity = a;
+ }
+
+ @JavascriptInterface
+ public void shareXlsx(String base64, String filename){
+
+  try{
+
+   byte[] data = Base64.decode(base64, Base64.DEFAULT);
+
+   File file = new File(activity.getCacheDir(), filename);
+   FileOutputStream fos = new FileOutputStream(file);
+   fos.write(data);
+   fos.close();
+
+   Uri uri = FileProvider.getUriForFile(
+     activity,
+     activity.getPackageName()+".provider",
+     file
+   );
+
+   Intent intent = new Intent(Intent.ACTION_SEND);
+   intent.setType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+   intent.putExtra(Intent.EXTRA_STREAM, uri);
+   intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+   activity.startActivity(Intent.createChooser(intent,"Отправить файл"));
+
+  }catch(Exception e){
+   e.printStackTrace();
+  }
+
+ }
+
+}
+
+
  protected void onCreate(Bundle savedInstanceState) {{
   super.onCreate(savedInstanceState);
 
